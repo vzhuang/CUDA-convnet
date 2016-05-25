739f5d27d335edd06f410cfb68e88575c97505cf
tongcharlie <tongcharlie@gmail.com>
2016-05-24 17:18:10 -0700

    fix merge bugs

--- cpu/layer.hpp
+++ cpu/layer.hpp
@@ -8,18 +8,22 @@
 #define RELU 1
 
 
+
+class Activation {
+public:
+  int type;
+  Activation(int type_);
+  float activ(float x);
+  float deriv(float x);
+};
+
+
+
 class Layer {
 public:  
   virtual void forward_prop(Tensor * input, Tensor * output) = 0;
-<<<<<<< HEAD
-  virtual void back_prop(float **** input_grad, Dimensions * input_dimensions,
-      float **** output_grad, Dimensions * output_dimensions) = 0;
-  virtual void output_dim(Dimensions * input_dimensions, 
-			  Dimensions * output_dimensions) = 0;
-=======
   virtual void back_prop(Tensor * input_grad, Tensor * output_grad) = 0;
   virtual void output_dim(Dimensions * input_dims, Dimensions * output_dims) = 0;
->>>>>>> b2051a1d3f9e7faf8009bc2e9bc88d842689d0a7
   virtual void free_layer() = 0;
 };
 
@@ -44,20 +48,11 @@ public:
   void free_layer();
 };
 
-class Activation {
-public:
-  int type;
-  Activation(int type_);
-  float activ(float x);
-  float deriv(float x);
-};
-
 // class ActivationLayer : public Layer {
 
 //   // Use for backprop
 //   Tensor * last_input;
 
-<<<<<<< HEAD
 // public:
 //   int type;
 //   // activation types - ReLU, tanh, sigmoid?
@@ -71,16 +66,14 @@ public:
 // 		  Dimensions * output_dimensions);
 //   void free_layer();
 // };
-=======
-public: 
-  // activation types - ReLU, tanh, sigmoid?
-  ActivationLayer();
-  void forward_prop(Tensor * input, Tensor * output);
-  void back_prop(Tensor * input_grad, Tensor * output_grad);
-  void output_dim(Dimensions * input_dims, Dimensions * output_dims);
-  void free_layer();
-};
->>>>>>> b2051a1d3f9e7faf8009bc2e9bc88d842689d0a7
+// public: 
+//   // activation types - ReLU, tanh, sigmoid?
+//   ActivationLayer();
+//   void forward_prop(Tensor * input, Tensor * output);
+//   void back_prop(Tensor * input_grad, Tensor * output_grad);
+//   void output_dim(Dimensions * input_dims, Dimensions * output_dims);
+//   void free_layer();
+// };
 
 
 
@@ -118,15 +111,9 @@ public:
 
   FullyConnectedLayer(int num_neurons_, int input_dim_, int act_type);
   void forward_prop(Tensor * input, Tensor * output);
-<<<<<<< HEAD
   void back_prop(Tensor * input_error, Tensor * output_error);
-  void output_dim(Dimensions * input_dimensions, 
-      Dimensions * output_dimensions);
-=======
-  void back_prop(Tensor * input_grad, Tensor * output_grad);
   void output_dim(Dimensions * input_dims, Dimensions * output_dims);
   void free_layer();
->>>>>>> b2051a1d3f9e7faf8009bc2e9bc88d842689d0a7
 
   // flatten inputs
   void flatten(Tensor * input, Tensor * reshaped);
